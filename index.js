248b80c9910e69b6a15f07429b318382cc58b302
chamchimayo <chamchi.maayo@gmail.com>
2022-09-11 21:10:23 +0900

    로그아웃 기능 추가

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
-const {auth} = require('./middleware/auth')
+const { auth } = require('./middleware/auth')
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
@@ -78,6 +78,17 @@ app.get('/api/users/auth', auth, (req, res) => {
     role: req.user.role,
     image: req.user.image
   })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({_id: req.user._id},
+    {token: ""},
+    (err, user) => {
+      if(err) return res.json({success: false, err});
+      return res.status(200).send({
+        success: true
+      })
+    })
 })
 
 
